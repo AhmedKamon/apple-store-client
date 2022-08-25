1f60294f9ff048c68d5072c383c2632e35934e7c
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-24 23:47:00 -0700

    header not done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,19 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
     <header>
-        <div>
-            <Image  src="https://rb.gy/vsvv2o" layout='fill' objectFit='contain' />
+        <div className="flex items-center justify-center md:w-1/5 ">
+      <Link href="/">
+        <div className=" relative w-10 h-10 cursor-pointer opacity-75 transition hover:opacity-100 ">
+          <Image src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain" />
+        </div>
+      </Link>
         </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
