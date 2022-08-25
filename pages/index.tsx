ff631c98fcbf0cde9b881ad88565038645c1e033
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-24 23:20:49 -0700

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";;
+import Head from "next/head";import Header from "../components/Header";
+;
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +9,9 @@ const Home: NextPage = () => {
         <title>Apple Redesign</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>BISMILLHA</h1>
+      
+      <Header/>
+       
     </div>
   );
 };
