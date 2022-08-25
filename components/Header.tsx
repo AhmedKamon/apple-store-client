b3e80160091e2e62975039692a9a45003eaa5022
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-25 09:26:00 -0700

    header custom css

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,19 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-30 flex items-center w-full justify-between bg-[#e7ecee] p-4 " >
         <div className="flex items-center justify-center md:w-1/5 ">
       <Link href="/">
-        <div className=" relative w-10 h-10 cursor-pointer opacity-75 transition hover:opacity-100 ">
+        <div className=" relative w-5 h-10 cursor-pointer opacity-75 transition hover:opacity-100 ">
           <Image src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain" />
         </div>
       </Link>
+        </div>
+        <div className=" hidden md:flex items-center justify-center flex-1 space-x-8 " >
+          <a href="" className="headerLink" >Product</a>
+          <a href="" className="headerLink" >Explore</a>
+          <a href="" className="headerLink" >Support</a>
+          <a href="" className="headerLink" >Business</a>
         </div>
     </header>
   );
