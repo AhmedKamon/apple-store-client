eea0d3930c32d3c876f50d8c4f0ba5967a469fd1
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-25 23:22:40 -0700

    landing page product section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
 import type { NextPage } from "next";
-import Head from "next/head";import Header from "../components/Header";
+import Head from "next/head";
+import Header from "../components/Header";
 import Landing from "../components/Landing";
-;
-
 const Home: NextPage = () => {
   return (
-    <div >
+    <div>
       <Head>
         <title>Tech Store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <Header/>
 
-      <main>
-        <Landing/>
+      <Header />
+
+      <main className="relative h-[200vh] bg-[#e7ecee] ">
+        <Landing />
       </main>
-       
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1b1b1b]">
+        <h1 className="text-center text-white text-4xl font-medium tracking-wide md:text-5xl" >New Promos</h1>
+      </section>
     </div>
   );
 };
