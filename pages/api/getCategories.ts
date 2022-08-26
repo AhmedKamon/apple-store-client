1295d2189bdb1f7381968c6469bfa2143c5f8125
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-26 10:03:09 -0700

    sanity client

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,14 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {groq} from 'next-sanity'
+import {sanityClient} from '../../sanity'
+
+// query
+const query = groq`*[_type == 'category']{
+  _id,
+  ...
+}`
 
 type Data = {
   categories:Category[]
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
-//   const categories = await sani
+ 
+  const categories = await sanityClient.fetch(query)
+  res.status(200).json({categories})
 }
