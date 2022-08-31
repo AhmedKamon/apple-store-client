1d35c7b51870ff7e5d63ad069fc5ccd4788960f7
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-31 08:45:02 -0700

    redux selectors

--- redux/basketSlice.ts
+++ redux/basketSlice.ts
@@ -44,4 +44,8 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 // selectors for retriveing items in state to use in components
 export const selectBasketItems = (state: RootState) => state.basket.items
+export const selectBasketItemsWithId = (state: RootState, id:string) => state.basket.items.filter((item:Product) => item._id === id)
+export const selectBasketTotal = (state:RootState) =>{
+  state.basket.items.reduce((total: number, item:Product) => (total+= item.price), 0)
+}
 export default basketSlice.reducer;
