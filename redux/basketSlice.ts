321be29fe6c1493a72de8fdc6ae8e23f0ddb061f
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-08-31 00:12:20 -0700

    redux/toolkit

---
+++ redux/basketSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { Product } from "../typings";
+import { RootState } from "./store";
+
+export interface basketState {
+  items: Product[];
+}
+
+const initialState: basketState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state: basketState, action: PayloadAction<Product>) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (
+      state: basketState,
+      action: PayloadAction<{ id: string }>
+    ) => {
+      // find the index if remove item
+      const index = state.items.findIndex(
+        (item: Product) => item._id === action.payload.id
+      );
+
+      let newBasket = [...state.items];
+      // remove item from basket
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.log(`can not find product ${action.payload.id}`);
+      }
+      state.items = newBasket;
+    },
+    
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+// selectors for retriveing items in state to use in components
+export const selectBasketItems = (state: RootState) => state.basket.items
+export default basketSlice.reducer;
