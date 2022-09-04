4e609aff1bb57f6d0d3bbf75b39be954a80ab4a5
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-09-04 03:37:17 -0700

    checkouts

--- components/CheckoutProducts.tsx
+++ components/CheckoutProducts.tsx
@@ -15,11 +15,16 @@ function CheckoutProducts({items, id}:Props) {
         <Image src={urlFor(items[0].image[0]).url()} alt='products' objectFit="contain" layout="fill" />
         </div>
         <div className=" flex flex-1 items-end lg:items-center " >
-            <div>
-                <div>
-                    <h4>{items[0].title}</h4>
-                    <p>{items.length}</p>
+            <div className="space-y-4 " >
+                <div className="flex flex-col gap-x-8 md:flex-row md:items-center md:justify-center lg
+                text-2xl" >
+                    <h4 className="font-bold lg:w-95 text-2xl " >{items[0].title}</h4>
+                    <p className="flex items-end font-bold" >{items.length} {items.length > 1 ? 'items': 'item'} </p>
                 </div>
+                <p className="flex cursor-pointer items-end text-blue-500 hover:underline " >Show Product Details</p>
+            </div>
+            <div>
+                <h4>500</h4>
             </div>
         </div>
     </div>
