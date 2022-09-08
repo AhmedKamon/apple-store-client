3c5015f98f9884a6de021ca19d5bafc5f930271d
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-09-08 10:13:10 -0700

    stripe api

--- pages/api/checkout_session.ts
+++ pages/api/checkout_session.ts
@@ -2,13 +2,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 import { urlFor } from "../../sanity";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
-apiVersion:'2022-08-01'
-})
+import { Product } from "../../typings";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2022-08-01",
+});
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
- 
- 
+  if (req.method === "POST") {
+    const items: Product[] = req.body.items;
+    // data shap stripe want from us
+    const transformedItems = items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.title,
+          images: [urlFor(item.image[0]).url()],
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: 1,
+    }));
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
 }
